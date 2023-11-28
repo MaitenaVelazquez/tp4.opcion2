4970c3b590fcc5af5ff4b60a922ddbad0a313b0a
MaitenaVelazquez <maitevelazquez031@gmail.com>
2023-11-28 17:36:31 -0300

    primer comentario

---
+++ tp4.2.py
@@ -0,0 +1,25 @@
+#Ejercicio 1
+#lista= []
+#def numeros (numero1,numero2):
+#    for i in range (numero1,numero2+1):
+#        lista.append(i)
+#    return lista
+#print(numeros (1,20))
+
+#Ejercicio 2 
+
+lista= []
+def numeros (numero1, numero2):
+    for i in range (numero1+1,numero2):
+        if i % 2 ==0:
+            lista.append(i)
+    return lista
+
+print (numeros (1,20))
+
+#Ejercicio 3
+def funcion (cadena,caracter):
+
+
+#Ejercicio 4
+def funcion (param1, param2):
